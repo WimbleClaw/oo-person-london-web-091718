f0a3c07e59bd2080b5fcdfcccc66c8061047d7b3
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-22 19:44:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,14 @@
 class Person
-  attr_accessor :hygiene
+  attr_accessor :hygiene, :bank_account, :happiness, salary
   attr_reader :name
+
 
   def initialize(name)
     @name = name
     @hygiene = 8
+    @bank_account = $25
+    @happiness = 8
+    @salary = []
   end
 
 end
